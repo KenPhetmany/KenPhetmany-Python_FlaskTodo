d1f4c58e3264bfc4aa47f8728357d1333432a44f
Ken Phetmany <kenphetmanydev@gmail.com>
2022-03-15 22:09:50 +1100

    Added route for editing post, need to finish creating html file for create

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,5 @@
+from crypt import methods
+import re
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
@@ -51,3 +53,22 @@ def create():
             return redirect(url_for('home'))
 
     return render_template('create.html')
+
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         'WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('edit.html', post=post)
